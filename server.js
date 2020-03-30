8475441dafc2552df23715d15c65b4c205529c97
cshekar86 <gubbi.c@husky.neu.edu>
2020-03-29 22:19:13 -0400

    server changes

--- server.js
+++ server.js
@@ -5,4 +5,14 @@ app.use(express.static(__dirname + '/dist/wbdv-sp20-cshekar6-client-angular'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
                          '/dist/wbdv-sp20-cshekar6-client-angular/index.html'));});
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin",
+             "https://wbdv-sp20-cse-client-angular.herokuapp.com/");
+  res.header("Access-Control-Allow-Headers",
+             "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods",
+             "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 app.listen(process.env.PORT || 8080);
