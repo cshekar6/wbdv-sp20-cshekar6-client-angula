a97a7c55549fac565bdb51706aaacc42be639dd5
cshekar86 <gubbi.c@husky.neu.edu>
2020-03-29 22:43:38 -0400

    Server changes

--- src/app/services/LessonServiceClient.ts
+++ src/app/services/LessonServiceClient.ts
@@ -3,6 +3,6 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class LessonServiceClient {
   findLessonsForModule = (moduleID) =>
-    fetch(`http://wbdv-generic-server.herokuapp.com/api/cshekar86/modules/${moduleID}/lessons`)
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/cshekar86/modules/${moduleID}/lessons`)
       .then(response => response.json())
 }
